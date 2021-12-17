12180e2fc1de73c78c838043462061873a1757ca
bugbytes-io <bugbytes.io@gmail.com>
2021-12-17 12:37:18 +0000

    Starter code

---
+++ gdp_analysis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gdp_analysis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gdp_analysis.settings')
+
+application = get_asgi_application()
